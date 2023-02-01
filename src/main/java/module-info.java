86d524417a264036f46f0955f6c6f6ee4a0940ec
Joshua Perry <45966243+jpez-development@users.noreply.github.com>
2023-02-01 20:37:58 +0000

    updated

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.r0r5chach {
 
     opens com.r0r5chach to javafx.fxml;
     exports com.r0r5chach;
-    exports com.r0r5chach.r6;
-    exports com.r0r5chach.valorant;
+    exports com.r0r5chach.competitor;
+    exports com.r0r5chach.competitor.r6;
+    exports com.r0r5chach.competitor.valorant;
 }

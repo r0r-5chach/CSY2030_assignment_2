c058fbde2ff375920c1b1e1a35428c6d64980564
Joshua Perry <45966243+jpez-development@users.noreply.github.com>
2023-02-05 18:56:11 +0000

    tests created

---
+++ src/test/java/com/r0r5chach/CompetitorRowTest.java
@@ -0,0 +1,65 @@
+package com.r0r5chach;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.r0r5chach.competitor.Name;
+import com.r0r5chach.competitor.Rank;
+import com.r0r5chach.competitor.valorant.ValorantAgent;
+import com.r0r5chach.competitor.r6.R6Attacker;
+import com.r0r5chach.competitor.r6.R6Defender;
+
+public class CompetitorRowTest {
+    
+    @Test
+    public void competitorRowCompetitorTest() {
+        CompetitorRow cR = new CompetitorRow(101, new Name("Joshua Perry"), Rank.GOLD, new int[]{5,5,5,5,5,5});
+        assertEquals(101, cR.getPlayerNumber());
+        assertEquals("Joshua Perry", cR.getPlayerName());
+        assertEquals(Rank.GOLD, cR.getPlayerLevel());
+        assertEquals("5, 5, 5, 5, 5, 5", cR.getScores());
+    }
+
+    @Test
+    public void competitorRowValorantPlayerTest() {
+        CompetitorRow cR = new CompetitorRow(101, new Name("Joshua Perry"), Rank.GOLD, new int[]{5,5,5,5,5,5}, ValorantAgent.ASTRA);
+        assertEquals(ValorantAgent.ASTRA.getAgent(), cR.getFavoriteAgent());
+    }
+
+    @Test
+    public void competitorRowR6PlayerTest() {
+        CompetitorRow cR = new CompetitorRow(101, new Name("Joshua Perry"), Rank.GOLD, new int[]{5,5,5,5,5,5}, R6Attacker.GLAZ, R6Defender.CASTLE);
+        assertEquals(R6Attacker.GLAZ.getAttacker(), cR.getFavoriteAttacker());
+        assertEquals(R6Defender.CASTLE.getDefender(), cR.getFavoriteDefender());
+    }
+
+    @Test
+    public void competitorRowGetPlayerNumberTest() {
+        //Already tested in competitorRowCompetitorTest()
+    }
+    @Test
+    public void competitorRowGetPlayerNameTest() {
+        //Already tested in competitorRowCompetitorTest()
+    }
+    @Test
+    public void competitorRowGetPlayerLevelTest() {
+        //Already tested in competitorRowCompetitorTest()
+    }
+    @Test
+    public void competitorRowGetScoresTest() {
+        //Already tested in competitorRowCompetitorTest()
+    }
+    @Test
+    public void competitorRowGetFavoriteAgentTest() {
+        //Already tested in competitorRowValorantPlayerTest()
+    }
+    @Test
+    public void competitorRowGetFavoriteAttackerTest() {
+        //Already tested in competitorRowR6PlayerTest()
+    }
+    @Test
+    public void competitorRowGetFavoriteDefenderTest() {
+        //Already tested in competitorRowR6PlayerTest()
+    }    
+}
